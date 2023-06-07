7a185d3ab50739b9cca6dddf7b9955e305641ae6
MarkFalcutila <83932128+markfalcutila@users.noreply.github.com>
2023-06-07 16:24:45 -0700

    Add files via upload

--- contact-script.js
+++ contact-script.js
@@ -1,7 +1,6 @@
 function SendMail(){
     var params = {
         from_name : document.getElementById("fullName").value,
-        subject : document.getElementById("subject").value,
         email_id : document.getElementById("email").value,
         contact_no : document.getElementById("contactNo").value,
         message : document.getElementById("message").value,
